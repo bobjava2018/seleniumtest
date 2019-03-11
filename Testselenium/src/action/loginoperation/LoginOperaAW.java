ba77ef6f672d15ad1b01715d4da8565e61f17ca9
baibo <524027223@qq.com>
2019-03-11 20:09:08 +0800

    bob

--- Testselenium/src/action/loginoperation/LoginOperaAW.java
+++ Testselenium/src/action/loginoperation/LoginOperaAW.java
@@ -1,5 +1,12 @@
 package action.loginoperation;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 /**
  * 2019-3-30
  * 登录操作类
@@ -8,6 +15,79 @@ package action.loginoperation;
  */
 public class LoginOperaAW {
 	
+	public static  WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+//		System.setProperty("webdriver.gecko.driver", "src/driver/geckodriver.exe");
+//			
+//		
+//		System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//		
+//		driver=new FirefoxDriver();
+//		
+//		Thread.sleep(2000);
+//		driver.manage().window().maximize();
+//		Thread.sleep(2000);
+//		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+//		Thread.sleep(2000);
+//		driver.get("https://www.baidu.com");
+//		
+//		System.out.println("当前打开的页面是："+driver.getTitle());
+//		
+//		driver.quit();
+		
+		LoginOperaAW lg=new LoginOperaAW();
+//		
+//		lg.startChrome();
+		
+		lg.startIE();
+	}
 	  
-
+    public void startChrome() {
+	    System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
+		
+	
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		
+		driver.get("https://www.baidu.com");
+		
+		System.out.println("当前打开的页面是："+driver.getTitle());
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+    }
+    public void startIE() {
+	    System.setProperty("webdriver.ie.driver", "src/driver/IEDriverServer.exe");
+		
+	
+		driver=new InternetExplorerDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		
+		driver.get("https://www.baidu.com");
+		
+		System.out.println("当前打开的页面是："+driver.getTitle());
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+    }
 }
